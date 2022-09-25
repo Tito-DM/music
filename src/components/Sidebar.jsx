88c53979a78035618153c827bd6154e162e8b522
pyong <muandatito@gmail.com>
2022-09-25 22:52:27 +0100

    change color

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,16 +5,16 @@ import { useState } from 'react';
 import { logo } from '../assets';
 import { links } from '../assets/constants';
 
-const NavLinks = ({handleClick}) => (
+const NavLinks = ({ handleClick }) => (
   <div className="mt-10">
     {links.map((item) => (
       <NavLink
         key={item.name}
         to={item.to}
         className="flex flex-row justify-start
-        items-center my-8 text-sm font-medium text-gray-400
+        items-center my-8 text- font-medium text-gray-400
          hover:text-cyan-400 "
-         onClick={() => handleClick && handleClick(false)}
+        onClick={() => handleClick && handleClick(false)}
       >
         {item.name}
       </NavLink>
@@ -25,7 +25,7 @@ const NavLinks = ({handleClick}) => (
 const Sidebar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   return (
-    <div className="md:flex hidden flex-col py-10 px-4 bg-[#191624]">
+    <div className="md:flex hidden flex-col py-10 px-4 bg-[#171422] w-[10%]">
       <img src={logo} alt="logo" className="w-full h-14 object-contain" />
       <NavLinks />
     </div>
